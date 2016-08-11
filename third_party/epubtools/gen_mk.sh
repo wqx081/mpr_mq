11c8ac280bdebc451deac924a9771dc1cb38c530
wqx081 <wangqx@mpreader.com>
2016-08-11 20:29:20 +0800

    Done argumnet handle

--- third_party/epubtools/gen_mk.sh
+++ third_party/epubtools/gen_mk.sh
@@ -2,16 +2,34 @@
 
 # Author: wang qixiang (wangqx@mpreader.com)
 
-INPUT_DIR=$1
-OUTPUT_MF=$2
-
-if [ "x$INPUT_DIR" = "x" ]; then
-    INPUT_MF=.
+if [ -z "$1" ]; then
+    INPUT_DIR="."
+else
+    INPUT_DIR=$1
 fi
 
-if [ "x$OUTPUT_DIR" = "x" ]; then
+
+if [ -z "$2" ]; then
     OUTPUT_MF="Makefile"
+else
+    OUTPUT_MF=$2
 fi
 
-echo "$INPUT_MF : $OUTPUT_MF"
+#echo "$INPUT_DIR : $OUTPUT_MF"
 
+echo "Starting."
+
+CPP_SOURCES=`find $INPUT_DIR -regex '.*\.\(cpp\|cc\)'`
+C_SOURCES=`find $INPUT_DIR -regex '.*\.\(c\)'`
+HEADERS=`find $INPUT_DIR -regex '.*\.\(h\|hpp\)'`
+
+
+echo "$CPP_SOURCES"
+echo "================="
+echo "$C_SOURCES"
+echo "================="
+echo "$HEADERS"
+
+
+echo ""
+echo "Done."
